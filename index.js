68abca252b045161a315330c7bec65ba53ea11ec
McTrip777 <mcjakedude@gmail.com>
2019-03-22 08:15:31 -0700

    first sprint commit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+require('dotevn').config();
+
+const server = require('./server');
+
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING;
+
+server.listen(port, () => {
+    console.log(`${greeting} ${port}`)
+})
