7e1815c9a6908ed0adfd77b47a25870600ecde40
McTrip777 <mcjakedude@gmail.com>
2019-03-22 10:03:21 -0700

    CRUD for Actions complete

--- data/router/actionRouter.js
+++ data/router/actionRouter.js
@@ -35,7 +35,8 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const action = await Action.insert(req.params.body);
+        const action = await Action.insert(req.body);
+        console.log(action);
             res.status(201).json(action);
     } 
     catch (error) {
@@ -46,8 +47,37 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = await Action.remove(req.params.id);
+        if(id > 0){
+            res.status(200).json({message: 'Action has been deleted'})
+        }else{
+            res.status(404).json({message: 'Action not found'})
+        }   
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          message: 'Error removing action',
+        });
+    }
+});
 
-
-
+router.put('/:id', async (req, res) => {
+    try {
+        const action = await Action.update(req.params.id, req.body);
+        if (action) {
+          res.status(200).json(action);
+        } else {
+          res.status(404).json({ message: 'Action could not be found' });
+        }
+      } catch (error) {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+          message: 'Error updating the action',
+        });
+      }
+});
 
 module.exports = router;
