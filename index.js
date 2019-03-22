ca7d20e29babda123e1f5447f35d8ed85353efda
McTrip777 <mcjakedude@gmail.com>
2019-03-22 08:53:35 -0700

    Working on action get

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
-require('dotenv').config();
-
 const server = require('./server.js');
 
-const port = process.env.PORT || 4000;
-const greeting = process.env.GREETING;
-
-server.listen(port, () => {
-    console.log(`${greeting} server is running on http://localhost${port}`)
+server.listen(4000, () => {
+    console.log(`server is running on http://localhost4000`)
 })
